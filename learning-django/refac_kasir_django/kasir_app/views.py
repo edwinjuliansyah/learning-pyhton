8a1793dc37fc796c4a714d57118e65d42084ee17
Emun <edwinjuliansyah273@gmail.com>
2026-08-18 19:58:24 +0700

    testhtml

--- learning-django/refac_kasir_django/kasir_app/views.py
+++ learning-django/refac_kasir_django/kasir_app/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse(f'===Warkop Emun===')
+    html = """
+        <html>
+        <head>
+        </head>
+        <body style="background-color:black;">
+        <h1 style="color: white; text-align: center;">Warkop Emun</h1>
+        </body>
+        </html>
+
+
+    """
+    return HttpResponse(html)
--- learning-django/refac_kasir_django/warkop_project/urls.py
+++ learning-django/refac_kasir_django/warkop_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('kasir_app.urls'))
+    path('', include('kasir_app.urls')),
 ]
